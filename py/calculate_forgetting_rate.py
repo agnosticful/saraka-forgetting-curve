f4eb87d9cf99c1227d44ffa49b398fd636a4880b
ryotogashi <ryo.togashi.ca@gmail.com>
2019-03-09 01:42:40 -0800

    Restracture

--- py/calculate_forgetting_rate.py
+++
@@ -1,9 +0,0 @@
-import math
-
-# Proportionality constant
-k = 1.84
-K = 1 / k
-c = 1.25
-
-def calculate_forgetting_rate(elapsed_minutes):
-    return (math.log10(elapsed_minutes))**c * 100 * K / (K * (math.log10(elapsed_minutes))**c + 1)
--- py/calculate_saving_rate.py
+++
@@ -1,9 +0,0 @@
-import math
-
-# Proportionality constant
-k = 1.84
-K = 1 / k
-c = 1.25
-
-def calculate_saving_rate(elapsed_minutes):
-    return 100 * k / ((math.log(elapsed_minutes))**c + k)
